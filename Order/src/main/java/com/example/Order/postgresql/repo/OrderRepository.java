d6d086318dbdab01cc3cda238e6b7f2c36e5f314
Federico Gallarati <federico.gallarat@edu.unito.it>
2023-02-09 14:29:52 +0100

    new version order-service stable

--- Order/src/main/java/com/example/Order/postgresql/repo/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Order.postgresql.repo;
-
-import com.example.Order.postgresql.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-
-}
