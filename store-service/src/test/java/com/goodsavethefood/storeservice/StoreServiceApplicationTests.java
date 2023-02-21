876504b14ccc8b63f88b2c9bf7f6a8493f1d29a0
Silvio Lanza <silvio.lanza@eng.it>
2023-02-21 17:22:30 +0100

    Fix StoreServiceApplicationTests.java of store service

--- store-service/src/test/java/com/goodsavethefood/storeservice/StoreServiceApplicationTests.java
+++ store-service/src/test/java/com/goodsavethefood/storeservice/StoreServiceApplicationTests.java
@@ -1,18 +1,10 @@
-<<<<<<<< HEAD:store-service/src/test/java/com/goodsavethefood/storeservice/StoreServiceApplicationTests.java
 package com.goodsavethefood.storeservice;
-========
-package com.gsf.userservice;
->>>>>>>> origin/customer-service:customer-service/src/test/java/com/gsf/userservice/UserserviceApplicationTests.java
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-<<<<<<<< HEAD:store-service/src/test/java/com/goodsavethefood/storeservice/StoreServiceApplicationTests.java
 class StoreServiceApplicationTests {
-========
-class UserserviceApplicationTests {
->>>>>>>> origin/customer-service:customer-service/src/test/java/com/gsf/userservice/UserserviceApplicationTests.java
 
 	@Test
 	void contextLoads() {
