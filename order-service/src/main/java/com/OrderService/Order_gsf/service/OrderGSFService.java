d9287babc63d213f3ac23510f4e9430ccb88e5e8
Silvio Lanza <silvio.lanza@eng.it>
2023-02-22 10:41:51 +0100

    Update

--- order-service/src/main/java/com/OrderService/Order_gsf/service/OrderGSFService.java
+++
@@ -1,9 +0,0 @@
-package com.OrderService.Order_gsf.service;
-
-import com.OrderService.Order_gsf.model.OrderGSF;
-import java.util.List;
-
-public interface OrderGSFService {
- public OrderGSF saveOrder(OrderGSF order);
- public List<OrderGSF> getAllOrder();
-}
